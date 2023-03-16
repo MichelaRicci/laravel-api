9d400f17456c7ea43fb766162ccfc61978a9330b
MichelaRicci <michela.ricci.1991@gmail.com>
2023-03-16 18:00:07 +0100

    test postman ok

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Project;
 
 class ProjectController extends Controller
 
@@ -14,7 +15,7 @@ class ProjectController extends Controller
      */
     public function index()
     {
-        $projects = Project::where('is_published', true)->get();
+        $projects = Project::all();
 
         return response()->json($projects);
     }
